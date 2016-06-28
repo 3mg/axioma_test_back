79f22e45aafd898b50cb567548ebb042e4def1b8
Nickolay Savchenko <3mg@bk.ru>
2016-06-29 02:37:53 +0300

    ~

--- load_fixtures.sh
+++ load_fixtures.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 php app/console doctrine:schema:drop -f
 php app/console doctrine:schema:update -f
-php app/console doctrine:fixtures:load --no-interaction
-php app/console hautelook_alice:doctrine:fixtures:load --append --no-interaction
+php app/console doctrine:fixtures:load --no-interaction --env=dev
+php app/console hautelook_alice:doctrine:fixtures:load --append --no-interaction --env=dev
